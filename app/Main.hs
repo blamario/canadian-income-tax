fc2046ce8ec476e755dd58f8ff8acef8bdf99826
Mario Blažević <blamario@protonmail.com>
2023-03-25 23:45:12 -0400

    The executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module Main where
 
+import Data.Text.IO qualified as Text.IO
+import System.Environment (getArgs)
+import Text.FDF (parse)
+
 import Tax.Canada.T1.FieldNames
+import Tax.Canada.T1.FDF qualified as FDF
 import Tax.Canada.T1.Types
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  [file] <- getArgs
+  fdf <- Text.IO.readFile file
+  case parse fdf >>= FDF.load of
+    Left err -> error err
+    Right t1 -> print t1
