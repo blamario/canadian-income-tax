9cb60d77abf1fe8276a8464ea708b958755c0c01
Mario Blažević <blamario@protonmail.com>
2023-04-02 08:32:05 -0400

    Finally updating the FDF with fixed values

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,9 @@ import Data.Text qualified as Text
 import System.Environment (getArgs)
 import Text.FDF (parse, serialize)
 
-import Tax.Canada.T1.FieldNames
 import Tax.Canada.T1.FDF qualified as FDF
+import Tax.Canada.T1.FieldNames
+import Tax.Canada.T1.Fix (fixT1)
 import Tax.Canada.T1.Types
 
 main :: IO ()
@@ -20,11 +21,12 @@ main = do
   case parse bytes >>= (\x-> (,) x <$> FDF.load x) of
     Left err -> error err
     Right (fdf, t1) -> do
-      let fdf' = FDF.update t1 fdf
+      let fdf' = FDF.update t1' fdf
+          t1' = fixT1 t1
           textPath = Text.pack path
           (prefix, extension) = evalState (Text.spanEndM beforeDot textPath) True
           beforeDot '.' = True <$ put False
           beforeDot c = get <* put True
           updatedPath = Text.unpack (prefix <> "-updated" <> extension)
       ByteString.writeFile updatedPath (serialize fdf')
-      print t1
+      print t1'
