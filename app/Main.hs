01bd52be8069d72f1c2b63b78b8d0f7ab988b85c
Mario Blažević <blamario@protonmail.com>
2023-04-08 13:23:39 -0400

    The executable works on either the T1 or ON428 form

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@
 
 module Main where
 
-import Control.Applicative ((<**>))
+import Control.Applicative ((<**>), optional)
 import Control.Monad (when)
 import Control.Monad.Trans.State.Strict (get, put, evalState)
 import Data.ByteString qualified as ByteString
@@ -14,7 +14,9 @@ import System.IO (hPutStrLn, stderr)
 import Text.FDF (parse, serialize)
 
 import Tax.FDF qualified as FDF
-import Tax.Canada.T1.FieldNames
+import Tax.Canada.ON428.FieldNames (on428Fields)
+import Tax.Canada.ON428.Fix (fixON428)
+import Tax.Canada.T1.FieldNames (t1Fields)
 import Tax.Canada.T1.Fix (fixT1)
 import Tax.Canada.T1.Types
 
@@ -23,7 +25,8 @@ main = execParser (info optionsParser $ progDesc "Update all calculated fields i
        >>= process
 
 data Options = Options {
-   inputPath :: FilePath,
+   t1InputPath :: Maybe FilePath,
+   on428InputPath :: Maybe FilePath,
    outputPath :: FilePath,
    verbose :: Bool}
 
@@ -31,20 +34,33 @@ data Options = Options {
 optionsParser :: Parser Options
 optionsParser =
    Options
-   <$> strArgument (metavar "<input FDF file>")
+   <$> optional (strOption (long "t1" <> metavar "<input T1 FDF file>"))
+   <*> optional (strOption (long "on428" <> metavar "<input ON428 FDF file>"))
    <*> strOption (short 'o' <> long "output" <> value "-" <> metavar "<output FDF file>")
    <*> switch (short 'v' <> long "verbose")
    <**> helper
 
 
 process :: Options -> IO ()
-process Options{inputPath, outputPath, verbose} = do
-   bytes <- if inputPath == "-" then ByteString.getContents else ByteString.readFile inputPath
-   case parse bytes >>= (\x-> (,) x <$> FDF.load t1Fields x) of
-      Left err -> error err
-      Right (fdf, t1) -> do
-         let fdf' = FDF.update t1Fields t1' fdf
-             t1' = fixT1 t1
-             write = if outputPath == "-" then ByteString.putStr else ByteString.writeFile outputPath
-         when verbose (hPutStrLn stderr $ show t1')
-         write (serialize fdf')
+process Options{t1InputPath, on428InputPath, outputPath, verbose} =
+   case (t1InputPath, on428InputPath) of
+      (Just path, Nothing) -> do
+         bytes <- if path == "-" then ByteString.getContents else ByteString.readFile path
+         case parse bytes >>= (\x-> (,) x <$> FDF.load t1Fields x) of
+            Left err -> error err
+            Right (fdf, form) -> do
+               let fdf' = FDF.update t1Fields form' fdf
+                   form' = fixT1 form
+                   write = if outputPath == "-" then ByteString.putStr else ByteString.writeFile outputPath
+               when verbose (hPutStrLn stderr $ show form')
+               write (serialize fdf')
+      (Nothing, Just path) -> do
+         bytes <- if path == "-" then ByteString.getContents else ByteString.readFile path
+         case parse bytes >>= (\x-> (,) x <$> FDF.load on428Fields x) of
+            Left err -> error err
+            Right (fdf, form) -> do
+               let fdf' = FDF.update on428Fields form' fdf
+                   form' = fixON428 form
+                   write = if outputPath == "-" then ByteString.putStr else ByteString.writeFile outputPath
+               when verbose (hPutStrLn stderr $ show form')
+               write (serialize fdf')
