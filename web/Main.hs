638befc50f86a4aad4fe7f5a270608e99e3c6dca
Mario Blažević <blamario@protonmail.com>
2023-11-10 10:46:42 -0500

    Serve completion of PDFs instead of FDFs

--- web/Main.hs
+++ web/Main.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
+import Control.Category ((>>>))
+import Control.Monad.IO.Class (liftIO)
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as ByteString
 import Data.ByteString.Lazy qualified as Lazy
 import Data.ByteString.Lazy qualified as ByteString.Lazy
 import Data.String (fromString)
-import Network.HTTP.Types.Status (ok200, unsupportedMediaType415)
+import Network.HTTP.Types.Status (ok200, internalServerError500, unsupportedMediaType415)
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Static
+import System.FilePath.Posix (combine)
 import Text.FDF (parse, serialize)
 import Web.Scotty
 
-import Tax.FDF qualified as FDF
+import Paths_canadian_income_tax (getDataDir)
 import Tax.Canada (fixOntarioReturns, fixON428, fixT1, on428Fields, t1Fields)
+import Tax.FDF qualified as FDF
+import Tax.PDFtk (fdf2pdf, pdf2fdf)
 
 main :: IO ()
 main = scotty 3000 $ do
@@ -24,14 +30,21 @@ main = scotty 3000 $ do
    get "/" $ do
       setHeader "Content-Type" "text/html; charset=utf-8"
       file "web/client/build/index.html"
-   post "/t1/FDF" $ do
-      bytes <- body
-      case parse (Lazy.toStrict bytes) >>= \fdf-> (,) fdf <$> FDF.load t1Fields fdf of
+   post "/t1/PDF" $ do
+      pdfBytes <- body
+      fdfBytes <- liftIO (pdf2fdf pdfBytes)
+      case fdfBytes >>= (Lazy.toStrict >>> parse) >>= \fdf-> (,) fdf <$> FDF.load t1Fields fdf of
         Left err -> status unsupportedMediaType415 >> text (fromString err)
         Right (fdf, form) -> do
           let fdf' = FDF.update t1Fields form' fdf
               form' = fixT1 form
-          status ok200
-          setHeader "Content-Type" "application/fdf"
-          raw (Lazy.fromStrict $ serialize fdf')
+          dataDir <- liftIO getDataDir
+          liftIO (fdf2pdf (combine dataDir "empty/5006-r-fill-22e.pdf") (Lazy.fromStrict $ serialize fdf')) >>= \case
+            Left err -> do
+              status internalServerError500
+              text (fromString err)
+            Right pdf' -> do
+              status ok200
+              setHeader "Content-Type" "application/fdf"
+              raw pdf'
    middleware $ staticPolicy (noDots >-> addBase "web/client/build")
--- web/client/src/App.js
+++ web/client/src/App.js
@@ -14,16 +14,16 @@ export default function Uploads() {
 
     function handleResponse (response) {
         if (response.ok) {
-            setT1output(response.body);
+            response.blob().then(setT1output);
         }
         else {
-            setError(response.body);
+            response.text().then(setError);
         }
     }
 
     function handleSubmit (event) {
         if (t1input) {
-            fetch("/t1/fdf", {"method": "POST", "mode": "same-origin", "body": t1input})
+            fetch("/t1/PDF", {"method": "POST", "mode": "same-origin", "body": t1input})
                 .then(handleResponse, setError)
                 .catch(setError);
             setSubmitted(true);
@@ -34,10 +34,10 @@ export default function Uploads() {
         <>
           <h2>T1 form completion</h2>
 
-          <input type="file" name="T1 FDF" onChange={handleUpload}/>
+          <input type="file" name="T1 PDF" onChange={handleUpload}/>
           <button name="Calculate" disabled={submitted !== false} onClick={handleSubmit}>Calculate</button>
             {t1output
-             ? <a name="Download" download="t1.fdf" href={console.log(t1output), URL.createObjectURL(new File(t1output, {name: "t1.fdf", type: 'application/FDF'}))}>Download</a>
+             ? <a name="Download" download="t1.pdf" href={console.log(t1output), URL.createObjectURL(new File([t1output], {name: "t1.pdf", type: 'application/PDF'}))}>Download</a>
              : <span>Download</span>}
             <p>{error.toString()}</p>
         </>
