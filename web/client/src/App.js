46d778f5ece1c8ba3c5f77e263d6c5baad997fae
Mario Blažević <blamario@protonmail.com>
2023-11-10 18:16:09 -0500

    Added province dropdown

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import './App.css';
 
 export default function Uploads() {
+    const [province, setProvince] = useState(null);
     const [t1input, setT1input] = useState(null);
     const [submitted, setSubmitted] = useState(null);
     const [t1output, setT1output] = useState(null);
@@ -30,16 +34,32 @@ export default function Uploads() {
         }
     }
 
-    return (
-        <>
-          <h2>T1 form completion</h2>
+    const provinces = [
+        {label: 'Alberta',          value: {code: 'AB', formPrefix: '5015'}},
+        {label: 'British Columbia', value: {code: 'BC', formPrefix: '5010'}},
+        {label: 'Ontario',          value: {code: 'ON', formPrefix: '5006'}},
+        {label: 'Quebec',           value: {code: 'QC', formPrefix: '5005'}}]
 
-          <input type="file" name="T1 PDF" onChange={handleUpload}/>
-          <button name="Calculate" disabled={submitted !== false} onClick={handleSubmit}>Calculate</button>
-            {t1output
-             ? <a name="Download" download="t1.pdf" href={console.log(t1output), URL.createObjectURL(new File([t1output], {name: "t1.pdf", type: 'application/PDF'}))}>Download</a>
-             : <span>Download</span>}
-            <p>{error.toString()}</p>
-        </>
+    return (
+      <>
+        <h2>T1 form completion</h2>
+
+        <h3>Step 1. <Dropdown className='provinceRoot' options={provinces} default={province} onChange={setProvince} placeholder="Select your province"/></h3>
+
+        {province && <>
+         <h3>Step 2. Download the <em>fillable</em> PDF form <tt>{province.value.formPrefix}-r-fill-22e.pdf</tt> from <a href="https://canada.ca">canada.ca</a></h3>
+         <h3>Step 3. Fill in the downloaded T1 form; don't bother with any fields that are calculated from other fields in the same form, that part will be performed automatically.</h3>
+         <h3>Step 4. Upload the filled form: <input type="file" name="T1 PDF" onChange={handleUpload}/></h3>
+         <h3>Step 5. <button name="Calculate" disabled={submitted !== false} onClick={handleSubmit}>Calculate</button></h3>
+         {t1output &&
+          <h3>Step 6. <a name="Download" download="t1.pdf"
+                         href={URL.createObjectURL(new File([t1output],
+                                                            {name: "t1.pdf", type: 'application/PDF'}))}>Download</a>
+          </h3>
+         }
+         </>
+        }
+        <p>{error.toString()}</p>
+      </>
     );
 }
