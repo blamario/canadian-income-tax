aa23497d8ad67e34cc631c98bdcbefd172b1fc75
Mario Blažević <blamario@protonmail.com>
2024-02-03 10:22:42 -0500

    Integrated Schedule 6 with no spouse

--- src/Tax/Canada/Federal.hs
+++ src/Tax/Canada/Federal.hs
@@ -14,6 +14,7 @@
 
 module Tax.Canada.Federal (Forms(..), fixFederalForms, formFieldsForProvince) where
 
+import Control.Applicative ((<|>))
 import Data.CAProvinceCodes qualified as Province
 import Data.Fixed (Centi)
 import Data.Text (Text)
@@ -22,16 +23,19 @@ import Rank2 qualified
 import Rank2.TH qualified
 import Transformation.Shallow.TH qualified
 
+import Tax.Canada.Federal.Schedule6 qualified as Schedule6
+import Tax.Canada.Federal.Schedule6 (Schedule6, fixSchedule6, schedule6Fields)
 import Tax.Canada.Federal.Schedule9 (Schedule9(line23_sum), fixSchedule9, schedule9Fields)
 import Tax.Canada.Federal.Schedule11 (Schedule11(page1), Page1(line6_trainingClaim, line17_sum), fixSchedule11, schedule11Fields)
 import Tax.Canada.T1 (fixT1, t1FieldsForProvince)
 import Tax.Canada.T1.Types (T1(page6, page8), Page6(line32300, line34900), Page8(step6_RefundOrBalanceOwing),
-                            Page8Step6(line_45350_CTC), LanguageOfCorrespondence, MaritalStatus)
+                            Page8Step6(line_45300_CWB, line_45350_CTC), LanguageOfCorrespondence, MaritalStatus)
 import Tax.FDF (Entry (Amount), FieldConst (Field), within)
 import Tax.Util (fixEq)
 
 data Forms line = Forms{
    t1 :: T1 line,
+   schedule6 :: Schedule6 line,
    schedule9 :: Schedule9 line,
    schedule11 :: Schedule11 line}
 
@@ -48,15 +52,19 @@ Rank2.TH.deriveAll ''Forms
 Transformation.Shallow.TH.deriveAll ''Forms
 
 fixFederalForms :: Forms Maybe -> Forms Maybe
-fixFederalForms = fixEq  $ \Forms{t1, schedule9, schedule11}-> Forms{
+fixFederalForms = fixEq  $ \Forms{t1, schedule6, schedule9, schedule11}-> Forms{
    t1 = fixT1 t1{page6 = t1.page6{line32300 = schedule11.page1.line17_sum, line34900 = schedule9.line23_sum},
                  page8 = t1.page8{step6_RefundOrBalanceOwing =
-                                  t1.page8.step6_RefundOrBalanceOwing{line_45350_CTC = schedule11.page1.line6_trainingClaim}}},
+                                  t1.page8.step6_RefundOrBalanceOwing{line_45300_CWB = schedule6.page4.step3.line42_sum <|>
+                                                                                       schedule6.page4.step2.line28_difference,
+                                                                      line_45350_CTC = schedule11.page1.line6_trainingClaim}}},
+   schedule6 = fixSchedule6 Nothing t1 schedule6,
    schedule9 = fixSchedule9 t1 schedule9,
    schedule11 = fixSchedule11 t1 schedule11}
 
 formFieldsForProvince :: Province.Code -> Forms FieldConst
 formFieldsForProvince p = Forms{
   t1 = within "T1" Rank2.<$> t1FieldsForProvince p,
+  schedule6 = within "Schedule6" Rank2.<$> schedule6Fields,
   schedule9 = within "Schedule9" Rank2.<$> schedule9Fields,
   schedule11 = within "Schedule11" Rank2.<$> schedule11Fields}
