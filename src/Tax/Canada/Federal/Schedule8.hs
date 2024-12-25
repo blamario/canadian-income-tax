a0a042d019812b0876abe70425536eebee3ff0b3
Mario Blažević <blamario@protonmail.com>
2024-12-24 21:40:48 -0500

    Added fixSchedule8

--- src/Tax/Canada/Federal/Schedule8.hs
+++ src/Tax/Canada/Federal/Schedule8.hs
@@ -66,7 +66,7 @@ data Page4 line = Page4{
 
 data Part4 line = Part4{
    line1_netSelfEmploymentEarnings :: line Centi,
-   line2_additionalEmploymentEarningsOffT4 :: line Centi,
+   line_50373_additionalEmploymentEarningsOffT4 :: line Centi,
    line3_sum :: line Centi,
    line4_basicExemption :: line Centi,
    line5_difference :: line Centi,
@@ -79,14 +79,13 @@ data Part4 line = Part4{
 
 data Page4Part5 line = Page4Part5{
    line1_netSelfEmploymentEarnings :: line Centi,
-   line2_additionalEmploymentEarningsOffT4 :: line Centi,
-   line3_additionalEmploymentEarningsOnT4 :: line Centi,
+   line_50373_additionalEmploymentEarningsOffT4 :: line Centi,
+   line_50399_additionalEmploymentEarningsOnT4 :: line Centi,
    line4_sum :: line Centi,
    line5_copy :: line Centi,
-   line6_copyIfPositive :: line Centi,
+   line6_copy :: line Centi,
    line7_difference :: line Centi,
-   line8_copy :: line Centi,
-   line8_fraction :: line Centi}
+   line8_fraction :: SubCalculation line}
 
 data Page5 line = Page5{
    line9_copy :: line Centi,
@@ -103,10 +102,8 @@ data Page5 line = Page5{
    line20_difference :: SubCalculation line,
    line21_difference :: SubCalculation line,
    line22_difference :: line Centi,
-   line23_copy :: line Centi,
-   line23_fraction :: line Centi,
-   line24_copy :: line Centi,
-   line24_double :: line Centi,
+   line23_fraction :: SubCalculation line,
+   line24_double :: SubCalculation line,
    line25_difference :: line Centi,
    line26_abs :: line Centi,
    line27_copy :: line Centi,
@@ -152,6 +149,87 @@ $(foldMap
        Transformation.Shallow.TH.deriveAll t])
    [''Schedule8, ''Page2, ''Page3, ''Page4, ''Page5, ''Page6, ''Part4, ''Page4Part5])
 
+fixSchedule8 :: Schedule8 Maybe -> Schedule8 Maybe
+fixSchedule8 = fixEq $ \Schedule8{page2, page3, page4, page5, page6}-> Schedule8{
+   page2 = page2,
+   page3 = let Page3{..} = page3 in page3{
+      line1_maxPensionableEarnings = ((5550 *) . fromIntegral . max 12) <$> page2.lineA_months,
+      line3_least = min line1_maxPensionableEarnings line_50339_totalPensionableEarnings,
+      line4_maxBasicExemption = ((/ 12) . (3500 *) . fromIntegral . min 12) <$> page2.lineA_months,
+      line5_difference = nonNegativeDifference line3_least line4_maxBasicExemption,
+      line7_fraction = fixSubCalculation (0.831933 *) line_50340_totalContributions,
+      line8_difference = nonNegativeDifference line_50340_totalContributions line7_fraction.result,
+      line9_fraction = fixSubCalculation (0.0495 *) line5_difference,
+      line10_fraction = fixSubCalculation (0.01 *) line5_difference,
+      line11_sum = totalOf [line9_fraction.result, line10_fraction.result],
+      line12_copy = line_50340_totalContributions,
+      line13_copy = line11_sum,
+      line14_difference = nonNegativeDifference line12_copy line13_copy},
+   page4 = Page4{
+      part4 = let Part4{..} = page4.part4 in page4.part4{
+         line3_sum = totalOf [line1_netSelfEmploymentEarnings, line_50373_additionalEmploymentEarningsOffT4],
+         line4_basicExemption = ((/ 12) . (3500 *) . fromIntegral . min 12) <$> page2.lineA_months,
+         line5_difference = nonNegativeDifference line3_sum line4_basicExemption,
+         line7_fraction = line6_contributionRate `fractionOf` line5_difference,
+         line8_fraction = fixSubCalculation (0.831933 *) line7_fraction,
+         line9_difference = nonNegativeDifference line7_fraction line8_fraction.result,
+         line10_fraction = fixSubCalculation (0.5 *) line8_fraction.result,
+         line11_sum = totalOf [line9_difference, line10_fraction.result]},
+      part5 = let Page4Part5{..} = page4.part5 in page4.part5{
+         line4_sum = totalOf [line1_netSelfEmploymentEarnings, line_50373_additionalEmploymentEarningsOffT4],
+         line5_copy = page3.line_50340_totalContributions,
+         line6_copy = page3.line14_difference,
+         line7_difference = nonNegativeDifference line5_copy line6_copy,
+         line8_fraction = fixSubCalculation (0.1680672 *) line7_difference}},
+   page5 = let Page5{..} = page5 in page5{
+      line9_copy = page3.line1_maxPensionableEarnings,
+      line10_copy = page3.line4_maxBasicExemption,
+      line11_difference = nonNegativeDifference line9_copy line10_copy,
+      line12_copy = page4.part5.line8_fraction.result,
+      line13_difference = nonNegativeDifference line11_difference line12_copy,
+      line14_least = min page4.part5.line4_sum line13_difference,
+      line15_copy = page3.line4_maxBasicExemption,
+      line16_copy = page3.line_50339_totalPensionableEarnings,
+      line17_difference = nonNegativeDifference line15_copy line16_copy,
+      line18_copy = page4.part5.line4_sum,
+      line19_copy = line11_difference,
+      line20_difference = fixSubCalculation id (nonNegativeDifference line18_copy line19_copy),
+      line21_difference = fixSubCalculation id (nonNegativeDifference line17_difference line20_difference.result),
+      line22_difference = nonNegativeDifference line14_least line21_difference.result,
+      line23_fraction = fixSubCalculation (0.119 *) line22_difference,
+      line24_double = fixSubCalculation (2 *) page3.line14_difference,
+      line25_difference = liftA2 (-) line23_fraction.result line24_double.result,
+      line26_abs = abs <$> line25_difference,
+      line27_copy = page3.line7_fraction.result,
+      line28_copy = page3.line9_fraction.result,
+      line29_difference = nonNegativeDifference line27_copy line28_copy,
+      line30_least = min line27_copy line28_copy,
+      line31_copy = page3.line8_difference,
+      line32_copy = page3.line10_fraction.result,
+      line33_difference = nonNegativeDifference line31_copy line32_copy,
+      line34_least = min line31_copy line32_copy},
+   page6 = let Page6{..} = page6 in page6{
+      line35_fraction = fixSubCalculation (0.5 *) (if any (< 0) page5.line25_difference then page5.line25_difference else Nothing),
+      line36_fraction = fixSubCalculation (0.831933 *) line35_fraction.result,
+      line37_difference = liftA2 (-) line35_fraction.result line36_fraction.result,
+      line38_copy = page5.line29_difference,
+      line39_copy = line36_fraction.result,
+      line40_difference = liftA2 (-) line38_copy line39_copy,
+      line41_copy = page5.line33_difference,
+      line42_copy = line37_difference,
+      line43_difference = liftA2 (-) line41_copy line42_copy,
+      line44_copy = if any (> 0) page5.line25_difference then page5.line25_difference else Nothing,
+      line45_fraction = fixSubCalculation (0.831933 *) line44_copy,
+      line46_difference = liftA2 (-) line44_copy line45_fraction.result,
+      line47_fraction = fixSubCalculation (0.5 *) line45_fraction.result,
+      line48_sum = liftA2 (+) line46_difference line47_fraction.result,
+      line49_copy = page5.line29_difference,
+      line50_copy = line47_fraction.result,
+      line51_sum = liftA2 (+) line49_copy line50_copy,
+      line52_copy = page5.line33_difference,
+      line53_copy = line48_sum,
+      line54_sum = liftA2 (+) line52_copy line53_copy}}
+
 schedule8Fields :: Schedule8 FieldConst
 schedule8Fields = within "form1" Rank2.<$> Schedule8{
    page2 = within "Page2" Rank2.<$> Page2{
@@ -178,7 +256,7 @@ schedule8Fields = within "form1" Rank2.<$> Schedule8{
    page4 = within "Page4" Rank2.<$> Page4{
       part4 = within "Part4" Rank2.<$> Part4{
          line1_netSelfEmploymentEarnings = Field ["Line1", "Amount"] Amount,
-         line2_additionalEmploymentEarningsOffT4 = Field ["Line2", "Amount"] Amount,
+         line_50373_additionalEmploymentEarningsOffT4 = Field ["Line2", "Amount"] Amount,
          line3_sum = Field ["Line3", "Amount"] Amount,
          line4_basicExemption = Field ["Line4", "Amount"] Amount,
          line5_difference = Field ["Line5", "Amount"] Amount,
@@ -190,14 +268,13 @@ schedule8Fields = within "form1" Rank2.<$> Schedule8{
          line11_sum = Field ["Line11", "amount"] Amount},
       part5 = within "part5" Rank2.<$> Page4Part5{
          line1_netSelfEmploymentEarnings = Field ["Line1", "Amount"] Amount,
-         line2_additionalEmploymentEarningsOffT4 = Field ["Line2", "Amount"] Amount,
-         line3_additionalEmploymentEarningsOnT4 = Field ["Line3", "Amount"] Amount,
+         line_50373_additionalEmploymentEarningsOffT4 = Field ["Line2", "Amount"] Amount,
+         line_50399_additionalEmploymentEarningsOnT4 = Field ["Line3", "Amount"] Amount,
          line4_sum = Field ["Line4", "Amount"] Amount,
          line5_copy = Field ["Line5", "Amount"] Amount,
-         line6_copyIfPositive = Field ["Line6", "Amount"] Amount,
+         line6_copy = Field ["Line6", "Amount"] Amount,
          line7_difference = Field ["Line7", "Amount"] Amount,
-         line8_copy = Field ["Line8", "Amount1"] Amount,
-         line8_fraction = Field ["Line8", "Amount2"] Amount}},
+         line8_fraction = subCalculationFields "Line8" ["Amount1"] ["Amount2"]}},
       page5 = within "Page5" . within "Part5_continued" Rank2.<$> Page5{
          line9_copy = Field ["Line9", "Amount"] Amount,
          line10_copy = Field ["Line10", "Amount"] Amount,
@@ -213,10 +290,8 @@ schedule8Fields = within "form1" Rank2.<$> Schedule8{
          line20_difference = subCalculationFields "Line20" ["Amount1"] ["Amount2"],
          line21_difference = subCalculationFields "Line21" ["Amount1"] ["Amount2"],
          line22_difference = Field ["Line22", "Amount"] Amount,
-         line23_copy = Field ["Line23", "Amount1"] Amount,
-         line23_fraction = Field ["Line23", "Amount2"] Amount,
-         line24_copy = Field ["Line24", "Amount1"] Amount,
-         line24_double = Field ["Line24", "Amount2"] Amount,
+         line23_fraction = subCalculationFields "Line23" ["Amount1"] ["Amount2"],
+         line24_double = subCalculationFields "Line24" ["Amount1"] ["Amount2"],
          line25_difference = Field ["Line25", "Amount"] Amount,
          line26_abs = Field ["Line26", "Amount"] Amount,
          line27_copy = Field ["Line27", "Amount"] Amount,
